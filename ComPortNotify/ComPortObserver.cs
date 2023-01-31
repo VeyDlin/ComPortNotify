00eaeaa4d43d4619cd3c8f96b0d988a8d099fcb1
nekto@veydlin.com <nekto@veydlin.com>
2023-01-31 09:07:11 +0300

    Add full name to Notify

--- ComPortNotify/ComPortObserver.cs
+++ ComPortNotify/ComPortObserver.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Ports;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +13,24 @@ public class ComPortObserver {
     private Task observerTask;
 
     public delegate void PortsChanged(List<string> addPorts, List<string> removePorts);
-    public PortsChanged onPortsChanged;
+    public PortsChanged? onPortsChanged;
 
 
     public ComPortObserver() {
-        ports = SerialPort.GetPortNames().ToList();
+        ports = GetPorts();
         observerTask = Task.Factory.StartNew(Observer, TaskCreationOptions.LongRunning);
     }
+
+
+    private List<string> GetPorts() {
+        using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'");
+        var portnames = SerialPort.GetPortNames();
+        var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+        var portList = portnames.Where(n => n != null).Select(n => ports.FirstOrDefault(s => s.Contains(n))).Cast<string>().ToList();
+
+        return portList;
+    }
+
 
 
 
@@ -25,14 +38,14 @@ public class ComPortObserver {
         while (true) {
             await Task.Delay(500);
 
-            var updatetPorts = SerialPort.GetPortNames().ToList();
+            var updatetPorts = GetPorts();
             var removePorts = ports.Except(updatetPorts).ToList();
             var addPorts = updatetPorts.Except(ports).ToList();
 
             ports = updatetPorts;
 
             if (removePorts.Count != 0 || addPorts.Count != 0) {
-                onPortsChanged.Invoke(addPorts, removePorts);
+                onPortsChanged?.Invoke(addPorts, removePorts);
             }
         }
     }
